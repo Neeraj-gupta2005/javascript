55beaee4cb05661e44fe3540c1e615a4031937cf
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-12-27 22:32:16 +0530

    playbar structure + hidding it and showing it

--- spotify/script.js
+++ spotify/script.js
@@ -2,6 +2,12 @@
 const player = document.getElementById("audio-player")
 const playButtons = document.querySelectorAll(".left-playbtn")
 
+const playbar = document.querySelector(".playbar")
+const leftSection = document.querySelector(".left-section");
+const rightSection = document.querySelector(".right-section");
+
+
+let isPlaying = false
 playButtons.forEach((playbutton) => {
     playbutton.addEventListener("click", () => {
         const filePath = playbutton.getAttribute("data-filePath")
@@ -25,19 +31,34 @@ playButtons.forEach((playbutton) => {
                 playIcon.style.display = "none"
                 pauseIcon.style.display = "block"
                 player.play()
+                isPlaying = true
             }
             else{
                 pauseIcon.style.display = "none"
                 playIcon.style.display = "block"
                 player.pause()
+                isPlaying = false
             }
         }else{
             player.src = filePath
-
             playIcon.style.display = "none"
             pauseIcon.style.display = "block"
             player.play()
+            isPlaying = true
         }
+
+        // to make playbar hide and show
+        // if (isPlaying === true) {
+        //     playbar.classList.add("show");
+        //     leftSection.style.paddingBottom = "60px";
+        //     rightSection.style.paddingBottom = "60px";
+        // } else {
+        //     playbar.classList.remove("show");  
+        //     leftSection.style.paddingBottom = "0px";
+        //     rightSection.style.paddingBottom = "0px";
+        // }
+
 
     })
 });
+
